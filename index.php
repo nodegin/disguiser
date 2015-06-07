d4fa9bb510b4f240826be57cca6526fd08599dad
‏ <nodegin@gmail.com>
2015-06-07 18:35:55 +0800

    fix indentation

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <html>
   <head>
     <title>hey world!</title>
-	<style>
-	  body{
-		  color: white;
-		  text-align: center;
-	  }
-	  .my-extremely-unbelievably-very-long-class-name{
-		  background: red;
-	  }
-	  .X_compare_with_this-long_long_longer-my-extremely-unbelievably-very-long-class-name{
-		  background: blue;
-	  }
-	</style>
+    <style>
+    body{
+      color: white;
+      text-align: center;
+    }
+    .my-extremely-unbelievably-very-long-class-name{
+      background: red;
+    }
+    .X_compare_with_this-long_long_longer-my-extremely-unbelievably-very-long-class-name{
+      background: blue;
+    }
+    </style>
   </head>
   <body>
     <div class="my-extremely-unbelievably-very-long-class-name">foo</div>
